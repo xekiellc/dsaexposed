115551f1ebb14de7f71e6d19d3bc20494d79bd27
xekiellc <zach@xekie.com>
2026-06-25 19:53:28 -0400

    Create footer.js

---
+++ components/footer.js
@@ -0,0 +1,11 @@
+<!-- TICKER -->
+<div class="ticker-wrap"></div>
+<script src="/components/ticker.js"></script>
+
+<!-- NAV -->
+<nav></nav>
+<script src="/components/nav.js"></script>
+
+<!-- FOOTER -->
+<footer></footer>
+<script src="/components/footer.js"></script>
